0780d72173db2da76484aba85d3e0be7d0b28887
Tim Roediger <superdweebie@gmail.com>
2014-09-05 21:50:19 +1000

    begin adding credits

---
+++ credits.js
@@ -0,0 +1,92 @@
+var credits = [
+    {
+        name: 'Tim Roediger',
+        roles: [
+            'lead designer'
+        ]        
+    },
+    {
+        name: 'Marty Olmos',
+        roles: [
+            'development advisor'
+        ]        
+    },
+    {
+        name: 'Josh Stuart',
+        roles: [
+            'topic review'
+        ]        
+    },
+    {
+        name: 'Liz Roediger',
+        roles: [
+            'play tester'
+        ]        
+    },
+    {
+        name: 'Miriam Roediger',
+        roles: [
+            'play tester'
+        ]        
+    },
+    {
+        name: 'Lucy Roediger',
+        roles: [
+            'play tester'
+        ]        
+    },
+    {
+        name: 'Toby Roediger',
+        roles: [
+            'play tester'
+        ]        
+    },
+    {
+        name: 'Louise Cowling',
+        roles: [
+            'play tester'
+        ]        
+    },
+    {
+        name: 'Jackson Watt',
+        roles: [
+            'play tester'
+        ]        
+    },
+    {
+        name: 'Patrick Khuu',
+        roles: [
+            'play tester'
+        ]        
+    },
+    {
+        name: 'Caitlin Murphy',
+        roles: [
+            'play tester'
+        ]        
+    },
+    {
+        name: 'Allan Chesher',
+        roles: [
+            'play tester'
+        ]        
+    },
+    {
+        name: 'Rona Bangahare',
+        roles: [
+            'play tester'
+        ]        
+    },
+    {
+        name: 'Allan Dalton',
+        roles: [
+            'play tester'
+        ]        
+    },
+    {
+        name: 'Jamie Lawrence',
+        roles: [
+            'play tester'
+        ]        
+    }    
+];
